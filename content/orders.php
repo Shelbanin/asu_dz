c7b12b3fa27d25013f606936585ba94df040c4df
Shelbanin <a.shelbanin@yandex.ru>
2016-03-05 13:53:41 +0300

    Таблица в заказах

--- content/orders.php
+++ content/orders.php
@@ -1,68 +1,54 @@
 <div id="content">
 	<div class="tab">
-		<table>
-			<tr>
-				<th>
-					№
-				</th>
-				<th>
-					ID заказа
-				</th>
-				<th>
-					Количество
-				</th>
-				<th>
-					Статус
-				</th>
-				<th>
-					Дата завершения
-				</th>
-				<th>
-					Действие
-				</th>
-			</tr>
-			
-			<tr class="even">
-				<td>
-					1
-				</td>
-				<td>
-					1362
-				</td>
-				<td>
-					150
-				</td>
-				<td>
-					В процессе
-				</td>
-				<td>
-					13.12.2016
-				</td>
-				<td>
-					<img src="./static/style/img/edit-icon.png">
-				</td>
-			</tr>
-			
-			<tr class="odd">
-				<td>
-					2
-				</td>
-				<td>
-					1322
-				</td>
-				<td>
-					200
-				</td>
-				<td>
-					Завершен
-				</td>
-				<td>
-					04.03.2016
-				</td>
-				<td>
-					<img src="./static/style/img/submit-icon.png">
-				</td>
-			</tr>	
-		</table>
+        <table>
+            <tr>
+                <th rowspan="2">№</th>
+                <th rowspan="2">ID заказа</th>
+                <th rowspan="2">Количество, шт</th>
+                <th rowspan="2">Статус</th>
+                <th rowspan="2">Прогресс, %</th>
+                <th colspan="2">Дата начала</th>
+                <th colspan="2">Дата завершения</th>
+            </tr>
+            <tr>
+                <th>ТЗ</th>
+                <th>Фактическая</th>
+                <th>ТЗ</th>
+                <th>Фактическая</th>
+            </tr>
+            <tr class="even">
+                <td>1</td>
+                <td>1352</td>
+                <td>100</td>
+                <td>Новый</td>
+                <td>-</td>
+                <td>05.04.2016</td>
+                <td>-</td>
+                <td>05.05.201</td>
+                <td>-</td>
+            </tr>
+            <tr class="odd">
+                <td>2</td>
+                <td>1364</td>
+                <td>150</td>
+                <td>В процессе</td>
+                <td>50</td>
+                <td>03.02.2016</td>
+                <td>03.02.2016</td>
+                <td>13.04.2016</td>
+                <td>-</td>
+            </tr>
+            <tr class="even">
+                <td>3</td>
+                <td>1314</td>
+                <td>50</td>
+                <td>Завершен</td>
+                <td>50</td>
+                <td>03.01.2016</td>
+                <td>03.01.2016</td>
+                <td>13.02.2016</td>
+                <td>13.02.2016</td>
+            </tr>
+        </table>
 	</div>
 </div>
