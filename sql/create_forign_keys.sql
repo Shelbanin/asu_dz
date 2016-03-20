b1f1efb24cc6f1e352f45bded06d57b069df0448
Shelbanin <a.shelbanin@yandex.ru>
2016-03-20 17:06:34 +0300

    Созданые первичные и вторичные ключи

---
+++ sql/create_forign_keys.sql
@@ -0,0 +1,15 @@
+﻿SPOOL create_tables.lst;
+PROMT Автор: Щелбанин Александр ВЛадимирович;
+PROMT Группа: ИУ4-83;
+PROMT Создание вторичных ключей БД для АСУ ТП изготовления RR-701-R-T;
+
+PROMT Создание вторичного ключа для ACCOUNTS;
+ALTER TABLE "ASU_USER"."ACCOUNTS" 
+    ADD (CONSTRAINT "ACC_USER_ID_FK" FOREIGN KEY("ACC_INFO") 
+    REFERENCES "ASU_USER"."USERS"("USER_ID") 
+    ON DELETE CASCADE);
+PROMT Создание вторичного ключа успешно завершено;
+
+
+PROMT Работа скрипта успешно завершена;
+SPOOL OFF;
