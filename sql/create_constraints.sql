9861483403e29b5fe2cc8d3c0492e96524ebbe1a
Shelbanin <a.shelbanin@yandex.ru>
2016-03-20 17:28:09 +0300

    Создание constraint

---
+++ sql/create_constraints.sql
@@ -0,0 +1,14 @@
+﻿SPOOL create_tables.lst;
+PROMT Автор: Щелбанин Александр ВЛадимирович;
+PROMT Группа: ИУ4-83;
+PROMT Создание ограничений БД для АСУ ТП изготовления RR-701-R-T;
+
+
+PROMT Создание ограничения C_USER_PHONE;
+ALTER TABLE "ASU_USER"."USERS" 
+    ADD (CONSTRAINT "C_USER_PHONE" CHECK(USER_PHONE LIKE 
+	'8-[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]'));
+PROMT Создание ограничения успешно завершено;
+			
+PROMT Работа скрипта успешно завершена;
+SPOOL OFF;
