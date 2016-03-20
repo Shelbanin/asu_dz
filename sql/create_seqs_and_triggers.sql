5224a2642ca3c94d757ffd7976d500412ebc4575
Shelbanin <a.shelbanin@yandex.ru>
2016-03-20 16:33:56 +0300

    Создание seqs & triggers для authorization

---
+++ sql/create_seqs_and_triggers.sql
@@ -0,0 +1,48 @@
+﻿SPOOL create_seqs_and_triggers.lst;
+PROMT Автор: Щелбанин Александр Владимирович;
+PROMT Группа: ИУ4-83;
+PROMT Создание последовательностей и триггеров БД для АСУ ТП изготовления RR-701-R-T;
+
+
+PROMT Создание последовательности для ACCOUNT.ACC_ID;
+DROP SEQUENCE "ASU_USER"."S_ACC_ID" ;
+CREATE SEQUENCE "ASU_USER"."S_ACC_ID" 
+	INCREMENT BY 1 START WITH 1
+	MAXVALUE 10000 MINVALUE 1 NOCYCLE 
+	NOCACHE NOORDER;
+PROMT Создание последовательности успешно завершено;
+
+PROMT Создание триггера для ACCOUNT.ACC_ID;
+CREATE OR REPLACE TRIGGER "ASU_USER"."T_ACC_ID" 
+	BEFORE INSERT ON "ASU_USER"."ACCOUNTS" 
+    FOR EACH ROW 
+BEGIN
+	SELECT s_acc_id.nextval
+		INTO :new.acc_id
+		FROM DUAL;
+END;
+PROMT Создание триггера успешно завершено;
+	
+	
+PROMT Создание последовательности для ACCOUNT.ACC_ID;
+DROP SEQUENCE "ASU_USER"."S_USER_ID" ;
+CREATE SEQUENCE "ASU_USER"."S_USER_ID" 
+	INCREMENT BY 1 START WITH 1
+	MAXVALUE 10000 MINVALUE 1 NOCYCLE 
+	NOCACHE NOORDER;
+PROMT Создание последовательности успешно завершено;
+
+PROMT Создание триггера для ACCOUNT.ACC_ID;
+CREATE OR REPLACE TRIGGER "ASU_USER"."T_USER_ID" 
+	BEFORE INSERT ON "ASU_USER"."USERS" 
+	FOR EACH ROW 
+BEGIN
+	SELECT s_user_id.nextval
+		INTO :new.user_id
+		FROM DUAL;
+END;
+PROMT Создание триггера успешно завершено;	
+	
+	
+PROMT Работа скрипта успешно завершена;
+SPOOL OFF;
