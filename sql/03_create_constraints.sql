dad8087eed775ee6ed06acc0f5fa9d7b421b7ba0
Shelbanin <a.shelbanin@yandex.ru>
2016-03-25 17:21:55 +0300

    login constraint

--- sql/03_create_constraints.sql
+++ sql/03_create_constraints.sql
@@ -4,6 +4,13 @@ PROMPT Группа: ИУ4-83;
 PROMPT Создание ограничений БД для АСУ ТП изготовления RR-701-R-T;
 
 
+PROMPT Создание ограничения C_ACC_LOGIN;
+ALTER TABLE ASU_USER.USERS
+  DROP CONSTRAINT C_ACC_LOGIN;
+ALTER TABLE ASU_USER.ACCOUNTS
+  ADD (CONSTRAINT C_ACC_LOGIN UNIQUE(ACC_LOGIN));
+PROMPT Создание ограничения успешно завершено;
+
 PROMPT Создание ограничения C_USER_PHONE;
 ALTER TABLE ASU_USER.USERS 
 	DROP CONSTRAINT C_USER_PHONE;
