e09d53f1c28b4a1e0f88c6ce8ba3137384c5e9f4
Shelbanin <a.shelbanin@yandex.ru>
2016-03-27 14:27:07 +0300

    Добавлены функции для rendrа таблиц

--- conf/config.php
+++ conf/config.php
@@ -1,5 +1,5 @@
 <?php
-require "console_log.php";
+require "../utils/console_log.php";
 
 function connect_to_db() {
     $db = "(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=alex-pc)(PORT=1521)))(CONNECT_DATA=(SID=ASUSH)))";
---
+++ utils/render_tables.php
@@ -0,0 +1,52 @@
+<?php
+function render_table($header, $simple_header, $fields, $data) {
+    if ($simple_header) {
+        render_header($header);
+    } else {
+        render_order_header();
+    }
+
+    $counter = 0;
+    while(OCIFetch($data)) {
+        render_row($fields, $data, $counter);
+        $counter++;
+    }
+}
+
+function render_header($header) {
+    echo "<tr>";
+    foreach ($header as $key => $value) {
+        echo "<th>" . $value . "</th>";
+    }
+    echo "</tr>";
+}
+
+function render_order_header() {
+    echo '
+        <tr>
+            <th rowspan="2">№</th>
+            <th rowspan="2">ID заказа</th>
+            <th rowspan="2">Количество, шт</th>
+            <th rowspan="2">Статус</th>
+            <th rowspan="2">Прогресс, %</th>
+            <th colspan="2">Дата начала</th>
+            <th colspan="2">Дата завершения</th>
+        </tr>
+        <tr>
+            <th>ТЗ</th>
+            <th>Фактическая</th>
+            <th>ТЗ</th>
+            <th>Фактическая</th>
+        </tr>
+    ';
+}
+
+function render_row($fields, $row, $counter) {
+    $style = !(bool)$counter % 2 ? 'even' : 'odd';
+    echo "<tr class=" . $style . ">";
+    foreach ($fields as $key => $value) {
+        echo "<td>" . OCIResult($row, $value) . "</td>";
+    }
+    echo "</tr>";
+}
+?>
