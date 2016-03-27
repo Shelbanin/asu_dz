fb96a7e04693df560ee224ca49dfec7458a25e88
Shelbanin <a.shelbanin@yandex.ru>
2016-03-27 13:11:41 +0300

    Добавлены запросы для отображения спрвочника

--- sql_queries/information.php
+++ sql_queries/information.php
@@ -1,8 +1,16 @@
 <?php
-    function docs_query() {
-        return "
-            SELECT doc_id, doc_name, doc_url, doc_description
-                FROM DOCUMENTS
-        ";
-    }
+function docs_query() {
+    return "
+        SELECT doc_id, doc_name, doc_url, doc_description
+          FROM documents
+    ";
+}
+
+function operations_query() {
+    return "
+        SELECT oper_id, oper_name, otyp_name, oper_description
+          FROM operations_info
+            JOIN operation_types ON oper_id = otyp_id
+    ";
+}
 ?>
