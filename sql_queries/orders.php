65ca2c76f8b822e370c31647aa9ab0203e8ca29d
Alexander Shelbanin <alexander.shelbanin@start-i.ru>
2016-04-09 10:56:17 +0300

    sql inserts

--- sql_queries/orders.php
+++ sql_queries/orders.php
@@ -1,4 +1,35 @@
 <?php
+function insert_into_dates($data) {
+    $query_to_insert = "
+      INSERT INTO dates (
+          date_start_spec,
+          date_end_spec
+	    )
+	    VALUES (
+		  '" . $data['date_start'] . "',
+		  '" . $data['date_end'] . "'
+	    )
+	";
+    return $query_to_insert;
+}
 
-
-?>s
+function insert_into_orders($data) {
+    $query_to_insert = "
+      INSERT INTO orders (
+		  ord_amount,
+		  ord_dates,
+		  ord_owner,
+		  ord_performer,
+		  ord_description
+	    )
+	    VALUES (
+		  '" . $data['amount'] . "',
+		  s_date_id.currval,
+		  '" . $data['owner'] . "',
+		  '" . $data['performer'] . "',
+		  '" . $data['description'] . "'
+	    )
+    ";
+    return $query_to_insert;
+}
+?>
