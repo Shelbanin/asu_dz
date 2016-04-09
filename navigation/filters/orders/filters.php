cfdc341196da5e31f544b9b9b5124382e00a2ee5
Alexander Shelbanin <alexander.shelbanin@start-i.ru>
2016-04-09 12:39:13 +0300

    фильтры

--- navigation/filters/orders/filters.php
+++ navigation/filters/orders/filters.php
@@ -6,10 +6,10 @@ $permission = get_permissions($_SESSION['user']['role'], 'orders');
 <div id="filters">
     <div class="left-block">
         <form action="" method="GET">
-            <button class="<? echo $filters['all'] ?>">Все</button>
-            <button class="<? echo $filters['new'] ?>">Новые</button>
-            <button class="<? echo $filters['in_progress'] ?>">Выполняемые</button>
-            <button class="<? echo $filters['done'] ?>">Завершенные</button>
+            <button class="<? echo $filters['all'] ?>" name="filter" value="all">Все</button>
+            <button class="<? echo $filters['new'] ?>" name="filter" value="new">Новые</button>
+            <button class="<? echo $filters['in_progress'] ?>" name="filter" value="in_progress">Выполняемые</button>
+            <button class="<? echo $filters['done'] ?>" name="filter" value="done">Завершенные</button>
         </form>
     </div>
 
